000d2c081469bd1b01ec4b6d0d51e5e0682519c4
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2023-12-30 13:27:16 +0530

    Welcome to Line Computation

---
+++ src/LineComparison.java
@@ -0,0 +1,7 @@
+public class LineComparison {
+    /*Line Computation Problem */
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Line Computation Problem");
+    }
+}
