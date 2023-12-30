59ffaebfd453354a3eaed89de94e29dcaf7fcf86
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2023-12-30 14:47:57 +0530

    Equality of 2 Lines based on length of Line

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,18 +1,20 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class LineComparison {
     /*Line Computation Problem */
-    /*A Length as 2 Points (x1, y1) and (x2, y2) -
-    Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2)*/
+    /*UC2-check equality of two lines
+based on the end points, when two lines are the equal.
+Using Java equals method to check equality of 2 Lengths is preferable.*/
 
     public static void main(String[] args) {
         System.out.println("Welcome to Line Computation Problem");
 
-        int xOne, xTwo, yOne, yTwo; //Initialise variable
+        int xOne, xTwo, yOne, yTwo ,xThree,yThree,xFour,yFour; //Initialise variable
 
-        double distance;
+        double distanceOfLineOne,distanceOfLineTwo;
 
-        //User input of x1,y1,x2,y2 coordinates
+        //User input of x1,y1,x2,y2 coordinates of Line1
         Scanner sc = new Scanner(System.in);
 
         System.out.println("enter x1 Coordinate");
@@ -31,8 +33,39 @@ public class LineComparison {
 
         yTwo = sc.nextInt();
         //Calculate distance using formula.
-        distance= Math.sqrt((xTwo - xOne) * (xTwo - xOne) + (yTwo - yOne) * (yTwo - yOne));
+        distanceOfLineOne= Math.sqrt((xTwo - xOne) * (xTwo - xOne) + (yTwo - yOne) * (yTwo - yOne));
 
-        System.out.println("distancebetween" + "(" + xOne + "," + yOne + ")," + "(" + xTwo + "," + yTwo + ")==" + distance);
+        System.out.println("distancebetween" + "(" + xOne + "," + yOne + ")," + "(" + xTwo + "," + yTwo + ")==" + distanceOfLineOne);
+        System.out.println("----------------------------------------------------------");
+         //Calculate length of Line2
+
+        System.out.println("enter x3 Coordinate");
+
+        xThree = sc.nextInt();
+
+        System.out.println("enter y3 Coordinate");
+
+        yThree = sc.nextInt();
+
+        System.out.println("enter x4 Coordinate");
+
+        xFour= sc.nextInt();
+
+        System.out.println("enter y4 Coordinate");
+
+        yFour = sc.nextInt();
+        //Calculate distance using formula.
+        //Length of Line 2
+        distanceOfLineTwo= Math.sqrt((xFour - xThree) * (xFour - xThree) + (yFour - yThree) * (yFour - yThree));
+        System.out.println("distancebetween" + "(" + xThree + "," + yThree + ")," + "(" + xFour + "," + yFour + ")==" + distanceOfLineTwo);
+
+        //Check the equality of Two lines
+        if(Objects.equals(distanceOfLineOne, distanceOfLineTwo)) {
+            System.out.println("Length of Line1 and Line2 is Equal");
+        } else
+        {
+            System.out.println("Length of Line1 and Line2 is unequal");
+
+        }
     }
 }
